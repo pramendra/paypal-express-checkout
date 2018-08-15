6b98d72112efd10b3a06f9db916f35545512c97d
Pramendra Gupta <pramendra@gmail.com>
2018-08-15 22:26:52 +0900

    updated braintree

--- server/middlewares/braintree-token.js
+++ server/middlewares/braintree-token.js
@@ -6,13 +6,17 @@ module.exports = async (
   res,
   // next,
 ) => {
-  const gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: process.env.MERCHANTID,
-    publicKey: process.env.PUBLICKEY,
-    privateKey: process.env.PRIVATEKEY,
-  });
-  const { clientToken, success } = await gateway.clientToken.generate({});
-  if (success) res.json({ clientToken });
-  res.send(404);
+  try {
+    const gateway = braintree.connect({
+      environment: braintree.Environment.Sandbox,
+      merchantId: process.env.MERCHANTID,
+      publicKey: process.env.PUBLICKEY,
+      privateKey: process.env.PRIVATEKEY,
+    });
+    const { clientToken, success } = await gateway.clientToken.generate({});
+    if (success) res.json({ clientToken });
+    throw new Error('cant retrive token');
+  } catch (error) {
+    res.send(403);
+  }
 };
