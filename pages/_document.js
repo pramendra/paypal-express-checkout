12f30f81df82addf66ca8e7974ed190613178f23
Pramendra Gupta <pramendra@gmail.com>
2018-08-17 10:39:25 +0900

    added document

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// @flow
+import * as React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Express Checkout</title>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
